ac1f965a07ab8f6664835ec8b253a7ba5dccb261
Sarah <sssalshahrani@tahakom.com>
2024-03-01 13:55:01 +0300

    feat: chat with consultant
    - without firebase integration

---
+++ lib/views/components/Avatar.dart
@@ -0,0 +1,25 @@
+
+import 'package:flutter/material.dart';
+
+class Avatar extends StatelessWidget {
+  final double size;
+  final image;
+  final EdgeInsets margin;
+  Avatar({this.image, this.size = 50, this.margin = const EdgeInsets.all(0)});
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: margin,
+      child: Container(
+        width: size,
+        height: size,
+        decoration: new BoxDecoration(
+          shape: BoxShape.circle,
+          image: new DecorationImage(
+            image: AssetImage(image),
+          ),
+        ),
+      ),
+    );
+  }
+}
