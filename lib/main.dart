0f105cf361044f576a898309b6791a0b80ed51f4
Sarah <sssalshahrani@tahakom.com>
2024-03-04 09:07:52 +0300

    integrate chat page with firebase - to be enhanced

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'views/landing_page.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp();  
+runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
--- lib/views/chat_page.dart
+++ lib/views/chat_page.dart
@@ -1,26 +1,30 @@
+import 'dart:io';
 import 'dart:ui';
+import 'package:intl/intl.dart' as ui;
 
 import 'package:Shoar/constants/colors.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'widgets/avatar.dart';
 
 class ChatPage extends StatefulWidget {
   @override
-  _ChatPageState createState() => _ChatPageState();
+  Received createState() => Received();
 }
 
-class _ChatPageState extends State<ChatPage> {
-  @override
+const bool useEmulator = true;
+
+class Received extends State<ChatPage> {
+  String host = Platform.isAndroid ? '10.0.2.2' : 'localhost';
+  FirebaseFirestore db = FirebaseFirestore.instance;
+
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:
-          Colors.transparent, // Set Scaffold background color to transparent
-
+      backgroundColor: Colors.transparent,
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage(
-                "assets/images/background.png"), // Replace "assets/background_image.jpg" with your image path
+            image: AssetImage("assets/images/background.png"),
             fit: BoxFit.cover,
           ),
         ),
@@ -107,140 +111,145 @@ class _ChatPageState extends State<ChatPage> {
 
   Widget _bodyChat() {
     return Expanded(
-      child: Container(
-        padding: EdgeInsets.only(left: 25, right: 25, top: 25),
-        width: double.infinity,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(45), topRight: Radius.circular(45)),
-          color: Colors.white,
-        ),
-        child: ListView(
-          physics: BouncingScrollPhysics(),
-          children: [
-            _itemChat(
-              avatar: 'assets/images/reda.png',
-              chat: 0,
-              message: 'السلام عليكم ورحمة الله, مساك الله بالخير أ.رضا ',
-              time: '18.00',
-            ),
-            _itemChat(
-              chat: 0,
-              message:
-                  'عندي استفسار بخصوص الاستثمار في المنصات التقنية , هل تنصح فيه ؟ عندي سيولة مالية الحمدلله لكن محتار هل بتنمو مع الاستثمار أو لا؟',
-              time: '18.03',
-            ),
-            _itemChat(
-              avatar: 'assets/images/reda.png',
-              chat: 1,
-              message:
-                  ' وعليكم السلام ورحمة الله وبركاته, اهلا بك ومساك الله بالنور ',
-              time: '18.45',
-            ),
-            _itemChat(
-              avatar: 'assets/images/reda.png',
-              chat: 1,
-              message:
-                  'انصحك بدراسة وتحليل المخاطر قبل اتخاذ القرار الاستثماري ‏اغلبية شركات التقنية الناشئة تتصف بمخاطرعالية جداً وتتفاوت درجات المخاطر في نوعها (تقنية، تشريعية، قانونية،تشغيلية،مالية ،تشغيلية، السوق، السيولة،…) وحجمها ومداها من شركة إلى أخرى ‏وعليه فانه من المفترض ان تقوم بتحليل تلك المخاطر لمعرفة مدى درجة المخاطرة الممكن تحملها أو تحييدها ومدى تأثيرها على مستقبل الشركة والعوائد المتوقعة على الاستثمار',
-              time: '18.47',
-            ),
-            _itemChat(
-              chat: 0,
-              message: 'هل فيه فرصة استثمارية تنصح فيها؟ نثق برأيكم أ.رضا😎',
-              time: '18.53',
-            ),
-            _itemChat(
-              avatar: 'assets/images/reda.png',
-              chat: 1,
-              message:
-                  'عندك هذي الفرصة الاستثمارية في قطاع التقنية ‏(استثمار جريء) ‏النمو المتوقع ضعفين خلال اقل من  سنتين "فنون" مختصة بالتصوير واللوحات الفنية وعرض الاعمال الفنية  ',
-              time: '19.00',
-            ),
-            _itemChat(
-              avatar: 'assets/images/reda.png',
-              chat: 1,
-              message: 'الحد الأدنى للاستثمار ‏70,000 ريال',
-              time: '19.01',
-            ),
-            _itemChat(
-              chat: 0,
-              message: 'ممتاز ممتاز , الله يعطيك العافية ما قصرت',
-              time: '19.02',
-            ),
-            _itemChat(
-              avatar: 'assets/images/reda.png',
-              chat: 1,
-              message: 'الله يعافيك بالتوفيق',
-              time: '19.05',
-            ),
-          ],
-        ),
+      child: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('chat')
+            .orderBy('timestamp', descending: false)
+            .snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
+
+          return Container(
+              padding: EdgeInsets.only(left: 25, right: 25, top: 25),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(45),
+                    topRight: Radius.circular(45)),
+                color: Colors.white,
+              ),
+              child: ListView(
+                physics: BouncingScrollPhysics(),
+                children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                  Map<String, dynamic> data =
+                      document.data() as Map<String, dynamic>;
+                  return _itemChat(
+                    avatar:
+                        'assets/images/reda.png', // Update with actual avatar
+                    chat: data['uid'], // Update with actual chat direction
+                    message: data['text'],
+                    time: data['timestamp'].toString(),
+                  );
+                }).toList(),
+              ));
+        },
       ),
     );
   }
 
   // 0 = Send
-  // 1 = Recieved
-  Widget _itemChat({int? chat, String? avatar, message, time}) {
-    return Row(
-      mainAxisAlignment:
-          chat == 0 ? MainAxisAlignment.end : MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: [
-        chat == 1
-            ? Avatar(
-                image: avatar,
-                size: 40,
-              )
-            : SizedBox(),
-        Flexible(
-          child: Container(
-            margin: EdgeInsets.only(
-              left: chat == 0 ? 10 : 0,
-              right: chat == 1 ? 10 : 0,
-              top: 20,
-            ),
-            padding: EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              color: chat == 0
-                  ? primaryColorTurquoise.withOpacity(0.1)
-                  : primaryColorYellow.withOpacity(0.1),
-              borderRadius: chat == 0
-                  ? BorderRadius.only(
-                      topLeft: Radius.circular(30),
-                      topRight: Radius.circular(30),
-                      bottomLeft: Radius.circular(30),
-                    )
-                  : BorderRadius.only(
-                      topLeft: Radius.circular(30),
-                      topRight: Radius.circular(30),
-                      bottomRight: Radius.circular(30),
-                    ),
-            ),
-            child: Column(
-              crossAxisAlignment:
-                  chat == 0 ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-              children: [
-                Directionality(
-                  textDirection: TextDirection.rtl,
-                  child: Text('$message'),
+  // 1 = Received
+  Widget _itemChat({
+  required String avatar,
+  required int chat,
+  required String message,
+  required String time, // Keep this as Timestamp
+}) {
+String timeFormatted = ui.DateFormat('kk:mm').format(
+                          DateTime.fromMillisecondsSinceEpoch(
+                              int.parse(time)));
+
+  return Row(
+    mainAxisAlignment: chat == 0 ? MainAxisAlignment.end : MainAxisAlignment.start,
+    crossAxisAlignment: CrossAxisAlignment.end,
+    children: [
+      chat == 1
+          ? Avatar(
+              image: avatar,
+              size: 40,
+            )
+          : SizedBox(),
+      Flexible(
+        child: Container(
+          margin: EdgeInsets.only(
+            left: chat == 0 ? 10 : 0,
+            right: chat == 1 ? 10 : 0,
+            top: 20,
+          ),
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: chat == 0
+                ? primaryColorTurquoise.withOpacity(0.1)
+                : primaryColorYellow.withOpacity(0.1),
+            borderRadius: chat == 0
+                ? BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                    bottomLeft: Radius.circular(30),
+                  )
+                : BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                    bottomRight: Radius.circular(30),
+                  ),
+          ),
+          child: Column(
+            crossAxisAlignment: chat == 0 ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+            children: [
+              Text('$message', textAlign: TextAlign.right),
+              SizedBox(height: 5),
+              Text(
+                '$timeFormatted',
+                style: TextStyle(
+                  color: Colors.grey.shade400,
+                  fontSize: 12,
                 ),
-                SizedBox(height: 5),
-                Text(
-                  '$time',
-                  style: TextStyle(
-                      color: Colors.grey.shade400,
-                      fontSize: 12), //todo: small size
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
-      ],
-    );
-  }
+      ),
+    ],
+  );
+}
 
   Widget _formChat() {
+    String messageText = ''; // Variable to store the text input
+    TextEditingController _messageController = TextEditingController();
+
+    void sendMessage() async {
+      String message = _messageController.text.trim();
+      if (message.isNotEmpty) {
+        if (useEmulator) {
+          db.useFirestoreEmulator(host, 8080);
+          db.settings = const Settings(
+            persistenceEnabled: false,
+          );
+        }
+        // Insert messageText into Firebase collection
+        FirebaseFirestore.instance.collection('chat').add({
+          'text': messageText,
+          'timestamp': DateTime.now().millisecondsSinceEpoch.toString(),
+          'uid': 1
+        }).then((value) {
+          // Clear the text field after sending message
+          setState(() {
+            messageText = '';
+          });
+        }).catchError((error) {
+          // Handle errors if any
+          print("Failed to send message: $error");
+        });
+        _messageController.clear();
+      }
+    }
+
     return Positioned(
       child: Align(
         alignment: Alignment.bottomCenter,
@@ -248,44 +257,40 @@ class _ChatPageState extends State<ChatPage> {
           height: 120,
           padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
           color: Colors.white,
-          child: TextField(
-            textAlign: TextAlign.right,
-            decoration: InputDecoration(
-              hintText: '...اكتب رسالتك',
-              prefixIcon: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  IconButton(
-                    icon: Icon(Icons.image_rounded),
-                    color: primaryColorTurquoise,
-                    onPressed: () {
-                      // Add image button action here
-                    },
+          child: Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _messageController,
+                  textAlign: TextAlign.right,
+                  onChanged: (value) {
+                    // Update messageText when text changes
+                    messageText = value;
+                  },
+                  decoration: InputDecoration(
+                    hintText: '...اكتب رسالتك',
+                    filled: true,
+                    fillColor: Colors.blueGrey[50],
+                    labelStyle: TextStyle(fontSize: 12),
+                    contentPadding: EdgeInsets.all(20),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                          color: const Color.fromRGBO(236, 239, 241, 1)),
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                          color: const Color.fromRGBO(236, 239, 241, 1)),
+                      borderRadius: BorderRadius.circular(25),
+                    ),
                   ),
-                  IconButton(
-                    icon: Icon(Icons.mic_rounded),
-                    color: primaryColorTurquoise,
-                    onPressed: () {
-                      // Add voice button action here
-                    },
-                  ),
-                ],
+                ),
               ),
-              filled: true,
-              fillColor: Colors.blueGrey[50],
-              labelStyle: TextStyle(fontSize: 12),
-              contentPadding: EdgeInsets.all(20),
-              enabledBorder: OutlineInputBorder(
-                borderSide:
-                    BorderSide(color: const Color.fromRGBO(236, 239, 241, 1)),
-                borderRadius: BorderRadius.circular(25),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderSide:
-                    BorderSide(color: const Color.fromRGBO(236, 239, 241, 1)),
-                borderRadius: BorderRadius.circular(25),
-              ),
-            ),
+              SizedBox(
+                  width: 10), // Add some spacing between text field and button
+              IconButton(
+                  icon: Icon(Icons.send), onPressed: () => sendMessage()),
+            ],
           ),
         ),
       ),
